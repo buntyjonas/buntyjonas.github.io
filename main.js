931b1ad0ea74eb47e2b6048c71b2ef5e68df4906
jonaslindstrom <jonas.lindstrom@hyperisland.se>
2016-05-19 20:32:16 +0200

    Push
    Test detect device

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(window).scroll(function() {
   
   var ySmall = ($(window).scrollTop() * smallSpeed); 
   
-  var smallPadding = navbarHeight - ySmall;
+  /*var smallPadding = navbarHeight - ySmall;
   if (smallPadding > navbarHeight) { smallPadding = navbarHeight; }
   if (smallPadding < smallLogoEndPos) { smallPadding = smallLogoEndPos; }
   if (smallPadding < 0) { smallPadding = 0; }
@@ -29,7 +29,7 @@ $(window).scroll(function() {
     $('.navbar').css({"box-shadow": "0 2px 3px rgba(0,0,0," + shadowOpacity + ")"});
   } else {
     $('.navbar').css({"box-shadow": "none"});
-  }
+  }*/
   
   
   
@@ -45,4 +45,24 @@ function MVPdesktop() {
     
     ga('send', 'event', 'orderDesktop', 'click', 'MVPdesktop');
 }
+
+var iosVariable = "sms:/ +4676-6862203 /&body=Vad behöver du:%0a%0aNär vill du ha det:%0a%0aVart vill du ha det:%0a%0aVad du tycker det är värt(ange värdet för ärendet exklusive eventuella inköp):";
+
+var androidVariable = "sms:/+4676-6862203 /?body=Vad behöver du:%0a%0aNär vill du ha det:%0a%0aVart vill du ha det:%0a%0aVad du tycker det är värt(ange värdet för ärendet exklusive eventuella inköp):"
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+  {
+    return 'iOS';
+document.getElementById("sms").href = iosVariable;
+  }
+  else if( userAgent.match( /Android/i ) )
+  {
+
+    return 'Android';
+      document.getElementById("sms").href = androidVariable;
+      
+  }
 
